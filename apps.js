b351024687e079d136329da7b8db96b5c35dcce6
LiKa12345 <46190241+LiKa12345@users.noreply.github.com>
2020-11-02 11:41:03 +0400

    fix: indents and some extra code

--- apps.js
+++ apps.js
@@ -1,118 +1,109 @@
+"use strict";
+
 class Task {
 	constructor(num, string) {
-    this.num = num;
-    this.string = string;
-	 }
-  }
-
-	class IntBuilder extends Task {
-	constructor(num) {
-    super(num);
-  
-     }  
-     plus() {
-        for (let i = 0; i < arguments.length; i++) {
-        this.num += arguments[i];
-        }
-        this.num;
-        return this;
-       }
-       
-     minus() {
-         for (let i = 0; i < arguments.length; i++) {
-             this.num -= arguments[i];
-         }
-         this.num;
-         return this;
-     }
-     
-     multiply(n) {
-        this.num *= n;
-        return this;
-    
-    }
-        divide(n) {
-        this.num /= n;
-        return this;
-    }
-
-        mod(n) {
-        this.num %= n;
-        return this;
-    }
-
-    static random(x, y) {
-        Math.floor(Math.random() * (y - x) + x);
-		return this;	
-    }
-    
-    get() {
-        
-        return this.num;
-    }
-
+		this.num = num;
+		this.string = string;
+	}
 }
 
-    const intBuilder = new IntBuilder(10);
+class IntBuilder extends Task {
+	constructor(num) {
+		super(num);
+	}
+	plus() {
+		for (let i = 0; i < arguments.length; i++) {
+			this.num += arguments[i];
+		}
+		return this;
+	}
 
-    intBuilder.plus(2, 3, 2).minus(1, 2).multiply(2).divide(4).mod(3).get();
-    IntBuilder.random(10, 100);
+	minus() {
+		for (let i = 0; i < arguments.length; i++) {
+			this.num -= arguments[i];
+		}
+		return this;
+	}
 
+	multiply(n) {
+		this.num *= n;
+		return this;
+	}
 
+	divide(n) {
+		this.num /= n;
+		return this;
+	}
+
+	mod(n) {
+		this.num %= n;
+		return this;
+	}
+
+	static random(x, y) {
+		let randValue = Math.floor(Math.random() * (y - x) + x);
+		return randValue;
+	}
+
+	get() {
+		return this.num;
+	}
+}
+
+const intBuilder = new IntBuilder(10);
+
+intBuilder.plus(2, 3, 2).minus(1, 2).multiply(2).divide(4).mod(3).get();
+IntBuilder.random(10, 100);
 
 
 //ESS
 
 
-    function StringBuilder(string) {
-        this.string = string;
-    }
-    
-    StringBuilder.prototipe = Object.create(Task.prototype);
-    StringBuilder.prototipe.constructor = StringBuilder;
-    StringBuilder.prototype._super = Task;
-    
-    
-    
-    StringBuilder.prototype.plus = function() {
-         for (let i = 0; i < arguments.length; i++) {
-         this.string += arguments[i];
-    }
-         return this;
-    }
-    
-    StringBuilder.prototype.minus = function(n) {
-        this.string = this.string.substring(0, this.string.length - n) + " ";
-        return this;
-    }
-    
-    StringBuilder.prototype.multiply = function(n) {
-        this.string = this.string.repeat(n);
-        return this;
-    }
-    
-    StringBuilder.prototype.divide = function(n) {
-        this.string = this.string.slice(0, n);
-        return this;
-    }
-    
-    StringBuilder.prototype.remove = function(n) {
-        this.string = this.string.split(n).shift();
-        return this;
-    }
-    
-    
-    StringBuilder.prototype.sub = function(x, y) {
-       this.string = this.string.substring(x, y + 1);
-        return this;
-    }
-    
-    
-    StringBuilder.prototype.get = function() {
-         return this.string; 
-    }
-    
-    var stringBuilder = new StringBuilder("Hello");
-    stringBuilder.plus(' all', '!').minus(4).multiply(3).divide(4).remove("l").sub(1, 1).get();
+function StringBuilder(string) {
+	this.string = string;
+}
 
-    
+StringBuilder.prototipe = Object.create(Task.prototype);
+StringBuilder.prototipe.constructor = StringBuilder;
+StringBuilder.prototype._super = Task;
+
+
+StringBuilder.prototype.plus = function () {
+	for (let i = 0; i < arguments.length; i++) {
+		this.string += arguments[i];
+	}
+	return this;
+}
+
+StringBuilder.prototype.minus = function (n) {
+	this.string = this.string.substring(0, this.string.length - n) + " ";
+	return this;
+}
+
+StringBuilder.prototype.multiply = function (n) {
+	this.string = this.string.repeat(n);
+	return this;
+}
+
+StringBuilder.prototype.divide = function (n) {
+	this.string = this.string.slice(0, n);
+	return this;
+}
+
+StringBuilder.prototype.remove = function (n) {
+	this.string = this.string.split(n).shift();
+	return this;
+}
+
+StringBuilder.prototype.sub = function (x, y) {
+	this.string = this.string.substring(x, y + 1);
+	return this;
+}
+
+StringBuilder.prototype.get = function () {
+	return this.string;
+}
+
+var stringBuilder = new StringBuilder("Hello");
+stringBuilder.plus(' all', '!').minus(4).multiply(3).divide(4).remove("l").sub(1, 1).get();
+
